ee21acff58215abf719ead7e47be3be50f53b845
gohilvaibhav88 <gohilvaibhav8844@gmail.com>
2023-12-28 15:48:41 +0530

    Your commit message here

---
+++ routes/index.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var router = express.Router();
+var userModel = require("./users");
+var postModel = require("./post");
+const passport = require('passport')
+const upload = require('./multer')
+
+const localStrategy = require("passport-local").Strategy;
+passport.use(new localStrategy(userModel.authenticate()));
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get("/login", function(req, res, next){
+  
+  res.render('login', {error: req.flash('error')});
+})
+
+router.get("/feed", function(req, res){
+  res.render('feed');
+})
+
+router.post('/upload', isLoggedIn, upload.single('file'),async function(req, res, next) {
+  if(!req.file){
+    return res.status(404).send("No files were given");
+  }
+  const user = await userModel.findOne({
+      username : req.session.passport.user
+      
+  });
+  const post = await postModel.create({
+    image: req.file.filename,
+    imageText : req.body.filecaption,
+    user : user._id
+  });
+  user.posts.push(post._id);
+  await user.save();
+  res.redirect("/profile");
+  
+});
+
+
+
+router.get('/profile', isLoggedIn ,async function(req, res, next) {
+
+  const users =await userModel.findOne({
+      username : req.session.passport.user
+  })
+  .populate("posts")
+  console.log(users)
+  res.render("profile", {users})
+});
+
+router.post("/register" , function(req, res){
+  const {username , email , fullname }= req.body;
+  const userData = new userModel({username , email , fullname});
+
+
+  userModel.register(userData , req.body.password).then(function(){
+    passport.authenticate("local")(req, res ,function(){
+      res.redirect("/profile");
+    })
+  })
+})
+
+router.post("/login" , passport.authenticate("local",{
+  successRedirect : "/profile", 
+  failureRedirect: "/login",
+  failureFlash: true
+}),function(req, res){
+  
+});
+
+router.get("/logout" , function(req, res){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+})
+
+function isLoggedIn(req , res , next){
+  if(req.isAuthenticated()){return next()}
+  res.redirect("/login");
+}
+
+
+
+
+
+module.exports = router;
