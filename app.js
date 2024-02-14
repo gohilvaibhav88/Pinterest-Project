e126c89bf8b901e008b03f7a1c66d7f55ca12eeb
Vaibhav Gohil <114984034+gohilvaibhav88@users.noreply.github.com>
2024-02-14 23:17:15 +0530

    first commit

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
+//HelloWorld
 app.use(function(req, res, next) {
   next(createError(404));
 });
